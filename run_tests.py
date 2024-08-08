5b4afa4423660ed43b2fab88a8c42d248dcfddb5
TommyBark <tomas.turlik@gmail.com>
2024-08-08 12:14:57 +0000

    Update tests script

--- run_tests.py
+++ run_tests.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import math
 
@@ -12,10 +13,16 @@ config = json.load(open("test_config.json"))
 TASK_NAMES = config["task_names"]
 BATCH_SIZE = config["batch_size"]
 model_config_path = config["model_config_path"]
+prompt_prefix = config["prompt_prefix"]
+padding_token = "<|endoftext|>"
+iterations = config["iterations"]
 
 
 def batch_answer(
-    texts: list, max_completion_length: int = 5, batch_size: int = 1
+    texts: list,
+    max_completion_length: int = 5,
+    batch_size: int = 1,
+    tqdm_disable: bool = False,
 ) -> list:
     # Ensure the input is a list of strings
     if not isinstance(texts, list):
@@ -26,7 +33,7 @@ def batch_answer(
     # Calculate the number of batches
     num_batches = math.ceil(len(texts) / batch_size)
 
-    for i in tqdm(range(num_batches)):
+    for i in tqdm(range(num_batches), disable=tqdm_disable):
         # Create a batch of inputs
         batch_texts = texts[i * batch_size : (i + 1) * batch_size]
 
@@ -34,6 +41,7 @@ def batch_answer(
         inputs = tokenizer(batch_texts, padding=True, return_tensors="pt").to(device)
 
         stop_token = r"\n"
+        # stop_token = "<|end|>"
         stop_token_id = tokenizer.encode(stop_token)[0]
         # Generate output for the current batch
         outputs = model.generate(
@@ -41,15 +49,15 @@ def batch_answer(
             max_length=max_completion_length + inputs["input_ids"].shape[1],
             eos_token_id=stop_token_id,
         )
-        #         # Decode the generated outputs to text
-        # decoded_outputs = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs]
-
-        # # Append the decoded outputs to the list of all outputs
-        # all_outputs.extend(decoded_outputs)
 
         for j, output in enumerate(outputs):
             # Decode the generated output to text
-            decoded_output = tokenizer.decode(output, skip_special_tokens=True)
+
+            decoded_output = tokenizer.decode(output, skip_special_tokens=False)
+
+            # remove padding tokens
+            while decoded_output.startswith(padding_token):
+                decoded_output = decoded_output.removeprefix(padding_token)
 
             # Index the output by the length of the input text, split on \n, and strip
             processed_output = (
@@ -64,7 +72,6 @@ def batch_answer(
 
 model, tokenizer, device = load_model_and_tokenizer(model_config_path)
 
-
 metrics = {}
 for task in TASK_NAMES:
     dataset = datasets.load_dataset("nguha/legalbench", task)
@@ -72,14 +79,23 @@ for task in TASK_NAMES:
     with open(f"tasks/{task}/base_prompt.txt") as in_file:
         prompt_template = in_file.read()
 
+    prompt_template = prompt_prefix + prompt_template
     test_df = dataset["test"].to_pandas()
     prompts = generate_prompts(prompt_template=prompt_template, data_df=test_df)
-    batch_generations = batch_answer(prompts, batch_size=BATCH_SIZE)
 
-    metric = evaluate(task, batch_generations, test_df["answer"].tolist())
+    score = 0.0
+    for _ in tqdm(range(iterations)):
+        batch_generations = batch_answer(
+            prompts, batch_size=BATCH_SIZE, tqdm_disable=True
+        )
+        metric = evaluate(task, batch_generations, test_df["answer"].tolist())
+        score += metric
+    metrics[task] = score / iterations
 
-    metrics[task] = metric
+current_datetime = datetime.datetime.now().strftime("%Y%m%d-%H%M")
+filename = f"results_{current_datetime}.txt"
 
-with open("results.txt", "w") as out_file:
+with open(filename, "w") as out_file:
+    out_file.write(f"Average of {iterations} runs\n")
     for task, metric in metrics.items():
         out_file.write(f"{task}: {metric}\n")
